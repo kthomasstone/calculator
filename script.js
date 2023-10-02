1bf42e7a022c64b1eebe5a3a1a7dde13c317f3b6
kthomasstone <22228480+kthomasstone@users.noreply.github.com>
2023-10-02 00:32:49 -0500

    create functions to link operations to html and clear totals.

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 let total = document.getElementById("total");
-
-let numberButtons = Array.from(
+const clearButton = document.getElementById("clear");
+const numberButtons = Array.from(
   document.getElementsByClassName("numbers-button")
 );
+const operationButtons = Array.from(
+  document.getElementsByClassName("operation")
+);
+const addButton = document.getElementById("add");
+const subtractButton = document.getElementById("subtract");
+const multiplyButton = document.getElementById("multiply");
+const divideButton = document.getElementById("divide");
+
+const equalButton = document.getElementById("equal");
 
 let operandA;
 let operator;
 let operandB;
+let answer;
 
-const clearButton = document.getElementById("clear");
+let operandACheck;
+
+// NUMBERS
 
 // when a number button is clicked, append that number to the total
 numberButtons.forEach((button) =>
@@ -20,15 +32,50 @@ function appendNumber(number) {
   if (total.textContent === "0") {
     clearTotal();
   }
+  // assign operand value
+  if (operator === undefined) {
+    if (operandA === undefined) {
+      operandA = number;
+      console.log(`operandA equals ${operandA}`);
+    } else {
+      operandA += number;
+      console.log(`operandA equals ${operandA}`);
+    }
+  } else {
+    if (operandB === undefined) {
+      operandB = number;
+      console.log(`operandB equals ${operandB}`);
+    } else {
+      operandB += number;
+      console.log(`operandB equals ${operandB}`);
+    }
+  }
+
   total.textContent += number;
   console.log(total.textContent);
 }
+
+// REMOVAL & CLEAR
 
 // clear the total when needed
 function clearTotal() {
   total.textContent = "";
 }
 clearButton.addEventListener("click", () => clearTotal());
+
+// OPERATIONS
+
+// create event listener for operations buttons. switch between cases depending on id of button clicked.
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => selectOperator(button.textContent));
+});
+
+function selectOperator(operation) {
+  operator = operation;
+  console.log(operator);
+  total.textContent += operation;
+}
 
 // Basic operation functions for add, subtract, multiply, and divide
 function add(a, b) {
@@ -49,6 +96,8 @@ function divide(a, b) {
 
 // Operate function calls upon basic operation functions depending on the operator selected
 function operate(a, operator, b) {
+  a = Number(a);
+  b = Number(b);
   switch (operator) {
     case "+":
       return add(a, b);
@@ -64,3 +113,12 @@ function operate(a, operator, b) {
       break;
   }
 }
+
+equalButton.addEventListener("click", () => {
+  answer = operate(operandA, operator, operandB);
+  console.log(answer);
+  operandA = undefined;
+  operandB = undefined;
+  operator = undefined;
+  total.textContent = answer;
+});
